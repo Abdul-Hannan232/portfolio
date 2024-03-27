aa40ade0abec51f54395d6f962f0d1a71b8ca6e8
Abdul-Hanan232 <abdulhani786@gmail.com>
2024-03-27 10:05:12 +0500

    project section

--- src/app/component/projects.js
+++ src/app/component/projects.js
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client"
+import React ,{useState}from 'react'
 import { IoMdArrowDropdown } from "react-icons/io";
 import Link from "next/link"
 import ProjectData from "../data/projects.json"
 import Image from "next/image"
 const Projects = () => {
+    const [displayCount, setDisplayCount] = useState(4); // State to keep track of the number of items to display
+
+    const handleViewMore = () => {
+        setDisplayCount(prevCount => prevCount + 4); // Increase display count by 4 when "View More" is clicked
+    };
     return (
         <div id="projects">
             <h1 className='md:text-5xl text-3xl font-bold text-center mt-44 mb-8'>Projects</h1>
@@ -21,27 +27,27 @@ const Projects = () => {
                     <Link href="#" >MobileApps</Link>
                 </div>
             </div> */}
-            <div style={{gridTemplateColumns:"auto auto", display:"grid"}} className=' md:w-[800px] my-5 mx-auto '>
-            {ProjectData.map((item,index)=>(
-                <div className=' my-3 mx-auto relative'>
-                 <div className={`${index % 2 === 0 ? ' blackBox': ' bg-[#2D8CFF] rounded-xl  md:w-[270px] md:mt-0 mt-5 w-[70%] mx-auto md:p-5 p-2 cursor-pointer'} `}>
-                    <Image src="/project1.png" alt="project1" width={461} height={213} className={ ` transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-105  duration-300 cursor-pointer${index % 2 === 0 ? "md:w-[461px] md:h-[213px] h-[160px]":"md:w-[225px] md:h-[213px] h-[180px]"}`} />
-               <Link href={item.link}>
-               <Image src="/export.png" alt="icon" width={16} height={18} className='w-auto  cursor-pointer absolute top-8 right-8' />
-
-                </Link>    
-                    <h1 className='md:text-[30px] text-2xl font-bold md:my-5 my-3 text-[#FFFFFF] poppins-bold '>{item.project_name}</h1>
-                    <button className='text-[#CACACA] bg-[#FFFFFF] bg-opacity-40  rounded-md poppins-semibold py-2  md:text-[11px] text-xs md:w-32 w-20 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-105  duration-300 cursor-pointer'>{item.category}</button>
-                </div>
-                </div>
-            ))}
+           <div style={{gridTemplateColumns:"auto auto", display:"grid"}} className='md:w-[800px] my-5 mx-auto'>
+                {ProjectData.slice(0, displayCount).map((item, index) => (
+                    <div key={index} className='my-3 mx-auto relative'>
+                        <div className={`${index % 2 === 0 ? 'blackBox shadow-xl' : 'bg-[#2D8CFF] rounded-xl md:w-[270px] md:mt-0 mt-5 w-[70%] mx-auto md:p-5 p-2 cursor-pointer'}`}>
+                            <Image src={item.image_name} alt="project1" width={461} height={213} className={`transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer${index % 2 === 0 ? "md:w-[461px] md:h-[213px] h-[160px]" : "md:w-[225px] md:h-[213px] h-[180px]"}`} />
+                            <Link href={item.link}>
+                                <Image src="/export.png" alt="icon" width={16} height={18} className='w-auto cursor-pointer  absolute bg-[#2D8CFF] p-1 rounded-md shadow-xl  top-8 right-8' />
+                            </Link>
+                            <h1 className='md:text-[30px] text-2xl font-bold md:my-5 my-3 text-[#FFFFFF] poppins-bold'>{item.project_name}</h1>
+                            <button className='text-[#CACACA] bg-[#FFFFFF] bg-opacity-40 rounded-md poppins-semibold py-2 md:text-[11px] text-xs md:w-32 w-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer'>{item.category}</button>
+                        </div>
+                    </div>
+                ))}
             </div>
-         
-            <div className='md:w-[800px] md:mx-auto mx-12 flex justify-center items-center gap-1 cursor-pointer bg-black text-white rounded-xl py-3 mt-3'>
-                <p>View More</p>
-                <IoMdArrowDropdown className='text-2xl' />
 
+            {displayCount < ProjectData.length && ( // Render "View More" button only if there are more items to display
+                <div className='md:w-[800px] md:mx-auto mx-12 flex justify-center items-center gap-1 cursor-pointer bg-black text-white rounded-xl py-3 mt-3' onClick={handleViewMore}>
+                    <p>View More</p>
+                    <IoMdArrowDropdown className='text-2xl' />
                 </div>
+            )}
         </div>
     )
 }
