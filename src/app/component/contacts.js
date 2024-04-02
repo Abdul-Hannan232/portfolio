118c10e7e13d8c945a3cfa615e8921c1e884a55a
Abdul-Hannan232 <abdulhani232@gmail.com>
2024-04-03 01:11:10 +0500

    set calendly button  hover effect

--- src/app/component/contacts.js
+++ src/app/component/contacts.js
@@ -8,14 +8,13 @@ const Contacts = () => {
         <motion.div whileInView={{ y: 0 }} initial={{ y:100 }} transition={{ delay: 0.2, duration: 1 }} id="contact"className='md:w-[780px] flex flex-wrap gap-3 mx-5 pt-56 md:mx-auto'>
             <div>
                 <div className='bg-[#2D8CFF] md:w-[350px] w-full py-10 px-7 rounded-2xl text-white'>
-                    <h1 className='text-white poppins-semibold md:text-2xl text-xl'>Book a Meeting with
-                        Calendly</h1>
+                    <h1 className='text-white poppins-semibold md:text-2xl text-xl'>Book a Meeting with Calendly</h1>
                     <p className='md:text-[12px] text-xs poppins-light my-1'>Feel free to adjust the wording to better fit<br/>
                         the tone and style of your website.</p>
                   <Link target="_blank" href="https://calendly.com/abdul-hannan-dev/60-minute-strategy-session?month=2024-03">
                     <div className='bg-[#FFFFFF] rounded-xl md:w-36 w-24 p-3 mt-4 flex items-center gap-2 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'>
                         <Image src="/icon.png" alt="icon" width={88} height={22}  className='w-auto'/>
-                        <Image src="/exportblack.png" alt="icon" width={16} height={18} className='w-auto' />
+                        <Image src="/exportblack.png" alt="icon" width={16} height={18} className='w-auto' /> 
                     </div>
                     </Link>
                   {/* <Link target="_blank" href="https://calendly.com/abdul-hannan-dev/60-minute-strategy-session?month=2024-03">
@@ -26,7 +25,7 @@ const Contacts = () => {
                     </Link> */}
                 </div>  
                 <div className='text-center md:w-[350px] w-full bg-[#EDEDED] py-14 mt-2 rounded-2xl'>
-                    <h1 className='text-[40px] poppins-bold'>Get in Touch</h1>
+                    <h1 className='text-[40px] poppins-bold'>Get in Touch </h1>
                     <p className='text-[#787878] text-sm w-64 mt-3 mx-auto'>Have a project in mind that you think we’d be a great fit for it? We’d love to know what you’re thinking</p>
                 </div>
             </div>
